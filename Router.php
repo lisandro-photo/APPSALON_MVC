92f91d70d7975b14d57448a0dbd67245994bce42
lisandro-photo <photo.lisandro.ar@gmail.com>
2022-07-06 16:46:21 -0500

    Cambios en Router

--- Router.php
+++ Router.php
@@ -7,25 +7,37 @@ class Router
     public array $getRoutes = [];
     public array $postRoutes = [];
 
-    public function get($url, $fn) {
+    public function get($url, $fn)
+    {
         $this->getRoutes[$url] = $fn;
     }
 
-    public function post($url, $fn) {
+    public function post($url, $fn)
+    {
         $this->postRoutes[$url] = $fn;
     }
 
-    public function comprobarRutas() {
+    public function comprobarRutas()
+    {
         $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' :  $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
 
-        if ($method === 'GET') {
-            $fn = $this->getRoutes[$currentUrl] ?? null;
-        } else {
-            $fn = $this->postRoutes[$currentUrl] ?? null;
-        }
+        // if ($method === 'GET') {
+        //     $fn = $this->getRoutes[$currentUrl] ?? null;
+        // } else {
+        //     $fn = $this->postRoutes[$currentUrl] ?? null;
+        // }
+        //dividimos la URL actual cada vez que exista un '?' eso indica que se están pasando variables por la url
+        $splitURL = explode('?', $currentUrl);
+        // debuguear($splitURL);
 
-        if ( $fn ) {
+        if ($method === 'GET') {
+            $fn = $this->getRoutes[$splitURL[0]] ?? null; //$splitURL[0] contiene la URL sin variables 
+        } else {
+          $fn = $this->postRoutes[$splitURL[0]] ?? null;
+        }
+        
+        if ($fn) {
             // Call user fn va a llamar una función cuando no sabemos cual sera
             call_user_func($fn, $this); // This es para pasar argumentos
         } else {
@@ -33,7 +45,8 @@ class Router
         }
     }
 
-    public function render($view, $datos = []) {
+    public function render($view, $datos = [])
+    {
         // Leer lo que le pasamos  a la vista
         foreach ($datos as $key => $value) {
             $$key = $value;  // Doble signo de dolar significa: variable variable, básicamente nuestra variable sigue siendo la original, pero al asignarla a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente

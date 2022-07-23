0c51510e3091eff349cf2206632dc645494e984f
Lisandro Arrua <106292851+lisandro-photo@users.noreply.github.com>
2022-07-22 20:47:15 -0500

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App Salón</title>
+    <link rel="shortcut icon" href="/build/img/1.jpg">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="/build/css/app.css">
 </head>
